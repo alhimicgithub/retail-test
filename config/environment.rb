ad7f08dd52ab56c175d6854ab5b1939ef67d2695
Dmytro Panov <dmitro.panov@gmail.com>
2015-08-10 19:43:21 +0300

    set logger level to debug

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActiveRecord::Base.logger.level = Logger::DEBUG
 # Initialize the Rails application.
 Rails.application.initialize!
